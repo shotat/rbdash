8e69e0354c1720688ddf8bb33c3bda2ee9bcb514
shotat <shotat@users.noreply.github.com>
2017-05-30 17:57:30 +0900

    update

--- lib/rbdash.rb
+++ lib/rbdash.rb
@@ -2,4 +2,8 @@ require 'rbdash/version'
 
 module Rbdash
   require 'rbdash/cli'
+  require 'rbdash/cli/init'
+  require 'rbdash/cli/pull'
+  require 'rbdash/cli/push'
+  require 'rbdash/models/query'
 end
--- lib/rbdash/cli/pull.rb
+++ lib/rbdash/cli/pull.rb
@@ -1,5 +1,10 @@
 module Rbdash
   class CLI::Pull
-
+    def run
+      queries = Rbdash::Models::Query.find_all
+      queries.each do |q|
+        q.save
+      end
+    end
   end
 end
